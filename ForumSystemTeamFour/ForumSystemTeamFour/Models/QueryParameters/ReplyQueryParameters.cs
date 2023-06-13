08cb17c1989b3319cb92f0938a5118c9038ce689
Dimitar Nedelchev <dimitar.nedelchev@dxc.com>
2023-06-13 18:54:33 +0300

    Added replies query parameters.

---
+++ ForumSystemTeamFour/ForumSystemTeamFour/Models/QueryParameters/ReplyQueryParameters.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ForumSystemTeamFour.Models.QueryParameters
+{
+    public class ReplyQueryParameters
+    {
+        public string? UserName { get; set; }
+        public string? Email { get; set; }
+        public DateTime? CreationDate { get; set; }
+    }
+}
