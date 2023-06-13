302789a975e879483baf58ab61f39fa8581930d0
Dimitar Nedelchev <dimitar.nedelchev@dxc.com>
2023-06-13 19:16:52 +0300

    Added reply read DTO.

---
+++ ForumSystemTeamFour/ForumSystemTeamFour/Models/DTOs/ReplyReadDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ForumSystemTeamFour.Models.DTOs
+{
+    public class ReplyReadDto
+    {
+        public int Id { get; set; }
+        public DateTime CreationDate { get; set; }
+        public int ThreadId { get; set; }
+        public int AuthorId { get; set; }
+        public User Author { get; set; }
+        public string Content { get; set; }
+        public int Likes { get; set; }
+        public int Dislikes { get; set; }
+    }
+}
