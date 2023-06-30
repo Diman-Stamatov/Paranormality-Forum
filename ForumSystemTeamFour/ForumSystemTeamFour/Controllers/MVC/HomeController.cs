fb3232e3a455b10cc0f541fa3aecb796645d95d7
Diman_Stamatov <d.stamatov@yahoo.com>
2023-06-30 17:05:04 +0300

    Added Bootstrap to the project

--- ForumSystemTeamFour/ForumSystemTeamFour/Controllers/MVC/HomeController.cs
+++ ForumSystemTeamFour/ForumSystemTeamFour/Controllers/MVC/HomeController.cs
@@ -1,15 +1,32 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using ForumSystemTeamFour.Services.Interfaces;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 
 namespace ForumSystemTeamFour.Controllers.MVC
 {
     [AllowAnonymous]
     public class HomeController : Controller
     {
+        private readonly IThreadService ThreadService;
+        public HomeController(IThreadService threadService)
+        {
+            this.ThreadService = threadService;
+        }
+
         [HttpGet]
         public IActionResult Index()
         {
-            return View();
+            if (!User.Identity.IsAuthenticated)
+            {
+                var threads = this.ThreadService.GetAll().OrderBy(thread=>thread.Comments.Count).ToList();
+                return View("AnonymousHome", threads);
+            }
+            else
+            {
+                return View("UserHome");
+            }
+            
         }
 
         [HttpGet]
