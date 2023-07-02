18f3ad235d35c0ac7b889d8bb39cdf1b380da8be
D1manko <d.stamatov@yahoo.com>
2023-07-02 17:02:21 +0300

    Fixed the Anonymous home page layout

---
+++ ForumSystemTeamFour/ForumSystemTeamFour/Controllers/MVC/UsersController.cs
@@ -0,0 +1,38 @@
+﻿using ForumSystemTeamFour.Services.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ForumSystemTeamFour.Controllers.MVC
+{
+    public class UsersController : Controller
+    {
+        private readonly IUserServices UserServices;
+        public UsersController(IUserServices userServices) 
+        {
+            this.UserServices = userServices;        
+        }
+
+        [HttpGet]
+        public IActionResult Profile([FromRoute] int id)
+        {
+            var userProfile = UserServices.GetById(id);
+            return View(userProfile);
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public IActionResult Create()
+        {            
+            return View();
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        
+    }
+}
