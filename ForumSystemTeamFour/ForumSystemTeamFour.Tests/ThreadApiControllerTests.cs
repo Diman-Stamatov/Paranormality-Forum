231c461b7c1bcb311cb063c6a1c1ff3d264601c3
Plamen Draganov <plamen.draganov@gmail.com>
2023-07-05 16:53:26 +0300

    fix ThreadApiController, TestModels, ThreadApiControllerTests

--- ForumSystemTeamFour/ForumSystemTeamFour.Tests/ThreadApiControllerTests.cs
+++ ForumSystemTeamFour/ForumSystemTeamFour.Tests/ThreadApiControllerTests.cs
@@ -20,7 +20,68 @@ using Moq;
 namespace ForumSystemTeamFour.Tests
 {
     [TestClass]
-    internal class ThreadApiControllerTests
+    public class ThreadApiControllerTests
     {
+        private void SetControllerContext(ThreadApiController controller)
+        {
+            var identity = new ClaimsIdentity();
+            identity.AddClaim(new Claim("LoggedUserId", DefaultId.ToString()));
+            var principal = new ClaimsPrincipal(identity);
+            var context = new ControllerContext { HttpContext = new DefaultHttpContext { User = principal } };
+
+            controller.ControllerContext = context;
+        }
+
+        [TestMethod]
+        public void CreateThread_ShouldCreate_WhenInputIsValid()
+        {
+            var mockUserServices = GetTestThreadServices().Object;
+            var mockSecurityServices = GetValidAuthenticationTestSecurity().Object;
+
+            var testedApi = new ThreadApiController(mockUserServices, mockSecurityServices);
+            SetControllerContext(testedApi);
+
+            var testCreateDto = GetTestThreadCreateDto();
+
+            var result = testedApi.CreateThread(testCreateDto) as ObjectResult;
+            var expectedCode = StatusCodes.Status201Created;
+            Assert.AreEqual(expectedCode, result.StatusCode);
+        }
+
+        [TestMethod]
+        public void CreateThread_ShouldThrow_WhenInputIsDuplicate()
+        {
+            var mockUserServices = GetTestThreadServices();
+            mockUserServices.Setup(services => services.Create(It.IsAny<ThreadCreateDto>(), It.IsAny<int>()))
+            .Throws<DuplicateEntityException>();
+
+            var mockSecurityServices = GetValidAuthenticationTestSecurity().Object;
+
+            var testedApi = new ThreadApiController(mockUserServices.Object, mockSecurityServices);
+            SetControllerContext(testedApi);
+
+            var testCreateDto = GetTestThreadCreateDto();
+
+            var result = testedApi.CreateThread(testCreateDto) as ObjectResult;
+            var expectedCode = StatusCodes.Status409Conflict;
+            Assert.AreEqual(expectedCode, result.StatusCode);
+        }
+
+        //[TestMethod]
+        //public void CreateToken_ShouldCreate_WhenInputIsValid()
+        //{
+        //    var mockUserServices = GetTestThreadServices().Object;
+        //    var mockSecurityServices = GetValidAuthenticationTestSecurity().Object;
+
+        //    var testedApi = new ThreadApiController(mockUserServices, mockSecurityServices);
+        //    SetControllerContext(testedApi);
+
+        //    string testUsername = "testLogin";
+        //    string testPassword = "testPassword";
+
+        //    var result = testedApi.Login(testUsername, testPassword) as ObjectResult;
+        //    var expectedCode = StatusCodes.Status201Created;
+        //    Assert.AreEqual(expectedCode, result.StatusCode);
+        //}
     }
 }
