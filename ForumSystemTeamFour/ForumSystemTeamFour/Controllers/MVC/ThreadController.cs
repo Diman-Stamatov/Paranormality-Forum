c25d433bb58350ff3f590195fb6d4a628df4317b
Plamen Draganov <plamen.draganov@gmail.com>
2023-07-12 13:19:45 +0300

    update: ThreadController

--- ForumSystemTeamFour/ForumSystemTeamFour/Controllers/MVC/ThreadController.cs
+++ ForumSystemTeamFour/ForumSystemTeamFour/Controllers/MVC/ThreadController.cs
@@ -58,17 +58,7 @@ namespace ForumSystemTeamFour.Controllers.MVC
         public IActionResult Create()
         {
             var threadCreateDto = new ThreadCreateDto();
-            try
-            {
-                threadCreateDto.AuthorId = int.Parse(User.Claims.FirstOrDefault(claim => claim.Type == "LoggedUserId").Value);
-            }
-            catch (UnauthorizedAccessException exception)
-            {
-                this.HttpContext.Response.StatusCode = StatusCodes.Status401Unauthorized;
-                this.ViewData["ErrorMessage"] = exception.Message;
-                return RedirectToAction("Login", "Users");
-            }
-            threadCreateDto.AuthorId = int.Parse(User.Claims.FirstOrDefault(claim => claim.Type == "LoggedUserId").Value);
+            threadCreateDto.AuthorId = int.Parse(User.Claims.FirstOrDefault(claim => claim.Type == "LoggedUserId").Value);            
             return this.View("Create", threadCreateDto);
         }
 
@@ -80,36 +70,23 @@ namespace ForumSystemTeamFour.Controllers.MVC
             {
                 return this.View(threadCreateDto);
             }
-            try
-            {
-                int loggedUserId = int.Parse(User.Claims.FirstOrDefault(claim => claim.Type == "LoggedUserId").Value);
-                var createdThread = this.ThreadServices.Create(threadCreateDto, loggedUserId);
-                return RedirectToAction("Details", new { id = createdThread.Id }); ;
-            }
-            catch (DuplicateEntityException exception)
-            {
-                this.ViewData["ErrorMessage"] = exception.Message;
-                this.HttpContext.Response.StatusCode = StatusCodes.Status409Conflict;
-                return this.View(threadCreateDto);
-            }
-            catch (UnauthorizedAccessException exception)
-            {
-                this.HttpContext.Response.StatusCode = StatusCodes.Status401Unauthorized;
-                this.ViewData["ErrorMessage"] = exception.Message;
-                return RedirectToAction("Login", "Users");
-            }
+            int loggedUserId = int.Parse(User.Claims.FirstOrDefault(claim => claim.Type == "LoggedUserId").Value);
+            var createdThread = this.ThreadServices.Create(threadCreateDto, loggedUserId);
+            return RedirectToAction("Details", new { id = createdThread.Id }); 
+
         }
 
         [Authorize]
         [HttpGet]
-        public IActionResult Update(int id)
+        public IActionResult Update([FromRoute] int id)
         {
             try
             {
                 var thread = this.ThreadServices.Details(id);
                 var threadUpdateDto = new ThreadUpdateDto();
                 string loggedUsername = User.Claims.FirstOrDefault(claim => claim.Type == "Username").Value;
-                if (loggedUsername != thread.Author.Username && !User.IsInRole("Admin"))
+                string loggedAdmin = User.Claims.FirstOrDefault(claim => claim.Type == "IsAdmin").Value;
+                if (loggedUsername != thread.Author.Username && loggedAdmin != "True")
                 {
                     throw new UnauthorizedAccessException("You are not authorized to update this thread.");
                 }
@@ -131,7 +108,7 @@ namespace ForumSystemTeamFour.Controllers.MVC
 
         [Authorize]
         [HttpPost]
-        public IActionResult Update(int id, ThreadUpdateDto threadUpdateDto)
+        public IActionResult Update([FromRoute] int id, ThreadUpdateDto threadUpdateDto)
         {
             if (!ModelState.IsValid)
             {
@@ -149,23 +126,18 @@ namespace ForumSystemTeamFour.Controllers.MVC
                 this.HttpContext.Response.StatusCode = StatusCodes.Status404NotFound;
                 return this.View(threadUpdateDto);
             }
-            catch (UnauthorizedAccessException exception)
-            {
-                this.HttpContext.Response.StatusCode = StatusCodes.Status401Unauthorized;
-                this.ViewData["ErrorMessage"] = exception.Message;
-                return this.View("Error401");
-            }
         }
 
         [Authorize]
         [HttpGet]
-        public IActionResult Delete(int id)
+        public IActionResult Delete([FromRoute] int id)
         {
             try
             {
-                string loggedUsername = User.Claims.FirstOrDefault(claim => claim.Type == "Username").Value;
+                
                 var threadToDelete = this.ThreadServices.Details(id);
-                if (loggedUsername != threadToDelete.Author.Username && !User.IsInRole("Admin"))
+                string loggedAdmin = User.Claims.FirstOrDefault(claim => claim.Type == "IsAdmin").Value;
+                if (loggedAdmin != "True")
                 {
                     throw new UnauthorizedAccessException("You are not authorized to delete this thread.");
                 }
@@ -173,6 +145,7 @@ namespace ForumSystemTeamFour.Controllers.MVC
                 {
                     throw new EntityNotFoundException("Post can not be found.");
                 }
+                //ToDo може да е view, което да пита дали искаш да изтриеш този Thread
                 return this.View("Delete", threadToDelete);
             }
             catch (EntityNotFoundException exception)
@@ -191,17 +164,16 @@ namespace ForumSystemTeamFour.Controllers.MVC
 
         [Authorize]
         [HttpPost]
-        public IActionResult Delete(int id, LargeThreadResponseDto threadUpdateDto)
+        public IActionResult Delete([FromRoute] int id, LargeThreadResponseDto threadUpdateDto)
         {
             try
             {
-                int loggedUserId = int.Parse(User.Claims.FirstOrDefault(claim => claim.Type == "LoggedUserId").Value);
-                var deletedThread = this.ThreadServices.Delete(id, loggedUserId);
-                if (loggedUserId != deletedThread.AuthorId && !User.IsInRole("Admin"))
+                string loggedAdmin = User.Claims.FirstOrDefault(claim => claim.Type == "IsAdmin").Value;
+                if (loggedAdmin != "True")
                 {
                     throw new UnauthorizedAccessException("You are not authorized to delete this thread.");
                 }
-                if (deletedThread.IsDeleted)
+                if (threadUpdateDto.isDeleted)
                 {
                     throw new EntityNotFoundException("Post can not be found.");
                 }
