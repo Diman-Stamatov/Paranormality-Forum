c22eda655a1f6af5c9f232d7009bf61f5fd4ac34
Dimitar Nedelchev <dimitar.nedelchev@dxc.com>
2023-07-16 17:32:45 +0300

    Added votes DB seeding.

---
+++ ForumSystemTeamFour/ForumSystemTeamFour/Models/Comparers/ReplyVoteComparer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+
+namespace ForumSystemTeamFour.Models.Comparers
+{
+    public class ReplyVoteComparer : IEqualityComparer<ReplyVote>
+    {
+        public bool Equals(ReplyVote x, ReplyVote y)
+        {
+            return x.Equals(y);
+        }
+
+        public int GetHashCode([DisallowNull] ReplyVote obj)
+        {
+            return obj.GetHashCode();
+        }
+    }
+}
